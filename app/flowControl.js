a38834d9cac79cb8a85bb95fb19f0b11aa9b5d7b
luchanso <oleg@loutchansky.com>
2016-04-22 14:20:20 +0300

    Complete flowControl

--- app/flowControl.js
+++ app/flowControl.js
@@ -2,6 +2,23 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.flowControlAnswers = {
   fizzBuzz : function(num) {
+    if (typeof num !== 'number') {
+        return false;
+    }
+
+    var result = num;
+
+    if (num % 3 === 0)
+        result = 'fizz';
+
+    if (num % 5 === 0)
+        result = 'buzz';
+
+    if (num % 3 === 0 && num % 5 == 0)
+        result = 'fizzbuzz';
+
+    return result;
+
     // write a function that receives a number as its argument;
     // if the number is divisible by 3, the function should return 'fizz';
     // if the number is divisible by 5, the function should return 'buzz';
